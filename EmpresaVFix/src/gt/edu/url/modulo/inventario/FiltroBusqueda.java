a2ae064a74a0416ceb03109a565bcd128df90262
CarlosMenchu2 <36994539+CarlosMenchu2@users.noreply.github.com>
2019-06-18 01:55:54 -0600

    Actualizacion del modulo inventario

---
+++ EmpresaVFix/src/gt/edu/url/modulo/inventario/FiltroBusqueda.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.url.modulo.inventario;
+
+/**
+ *
+ * @author carlo
+ */
+public class FiltroBusqueda {
+    
+    private String codigo;
+
+    public String getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+}
