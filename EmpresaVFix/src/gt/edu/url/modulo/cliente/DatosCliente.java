4ddf5e3fbcbc3f5bd969da964033e2d673d38cf1
CarlosMenchu2 <36994539+CarlosMenchu2@users.noreply.github.com>
2019-06-17 20:15:50 -0600

    Actualizacion de modulo Inventario

--- EmpresaVFix/src/gt/edu/url/modulo/cliente/DatosCliente.java
+++ EmpresaVFix/src/gt/edu/url/modulo/cliente/DatosCliente.java
@@ -11,52 +11,6 @@ package gt.edu.url.modulo.cliente;
  */
 public class DatosCliente {
     
-    private String codigo;
-    private String nombre;
-    private String telefono;
-    private String cumpleanios;
-    private String estrellas;
-    
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getCumpleanios() {
-        return cumpleanios;
-    }
-
-    public void setCumpleanios(String cumpleanios) {
-        this.cumpleanios = cumpleanios;
-    }
-
-    public String getEstrellas() {
-        return estrellas;
-    }
-
-    public void setEstrellas(String estrellas) {
-        this.estrellas = estrellas;
-    }
-    
+   
     
 }
